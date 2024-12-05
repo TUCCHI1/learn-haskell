6e3703afac84ba203640fd0f37e24a1729e25099
TUCCHI1 <tutikikin1@gmail.com>
2024-12-05 16:18:31 +0900

    HTML生成関数を追加し、タイトルを含むHTMLを作成するように修正

--- hello.hs
+++ hello.hs
@@ -1,8 +1,20 @@
-html_ content = "<html>" <> content <> "</html>"
-body_ content = "<body>" <> content <> "</body>"
+
 head_ content = "<head>" <> content <> "</head>"
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+el :: String -> String -> String
+el tag content = 
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+title_ :: String -> String
 title_ content = "<title>" <> content <> "</title>"
 
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 myhtml = makeHtml "Hello title" "Hello, world!"
