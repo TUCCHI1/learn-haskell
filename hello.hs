575cb42dcff31cd0721889fa4517f14c67647ef5
TUCCHI1 <tutikikin1@gmail.com>
2024-12-05 13:57:53 +0900

    HTML生成関数を拡張し、タイトルを含むHTMLを作成するように変更

--- hello.hs
+++ hello.hs
@@ -1,4 +1,11 @@
 html_ content = "<html>" <> content <> "</html>"
 body_ content = "<body>" <> content <> "</body>"
+head_ content = "<head>" <> content <> "</head>"
+title_ content = "<title>" <> content <> "</title>"
 
-myHtml = html_ (body_ "Hello, World!")
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+myhtml = makeHtml "Hello title" "Hello, world!"
+
+main :: IO ()
+main = putStrLn myhtml
