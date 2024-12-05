577a269afbb46640f56d9df04b2ebfa978cb9a59
TUCCHI1 <tutikikin1@gmail.com>
2024-12-05 16:19:45 +0900

    新しい関数を追加し、2つの数値を加算する機能を実装

--- hello.hs
+++ hello.hs
@@ -19,5 +19,7 @@ makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 myhtml = makeHtml "Hello title" "Hello, world!"
 
+three = (\num1 -> \num2 -> num1 + num2) 1 2
+
 main :: IO ()
 main = putStrLn myhtml
