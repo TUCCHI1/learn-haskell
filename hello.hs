a010ba3427f59c584632bd7c1b6837c2a3c60cfb
TUCCHI1 <tutikikin1@gmail.com>
2024-12-06 13:10:06 +0900

    HTML生成関数を修正し、main関数を追加して出力を改善

--- hello.hs
+++ hello.hs
@@ -1,14 +1,14 @@
+main :: IO ()
+main = putStrLn myhtml
+
 myhtml :: String
 myhtml =
-     makeHtml
-      "Hello, title"
-       (h1_ "Hello, world!" <> p_ "Let's learn about haskell!")
+    makeHtml
+        "Hello, title"
+        (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
 
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
-
-el :: String -> String -> String
-el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
 html_ :: String -> String
 html_ = el "html"
@@ -28,5 +28,6 @@ p_ = el "p"
 h1_ :: String -> String
 h1_ = el "h1"
 
-main :: IO ()
-main = putStrLn myhtml
+el :: String -> String -> String
+el tag content =
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
