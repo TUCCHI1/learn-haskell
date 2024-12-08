22a9735c31923e2d5be986bcae877390f52970b3
TUCCHI1 <tutikikin1@gmail.com>
2024-12-08 18:23:25 +0900

    hello.hsに安全な割り算関数を追加し、メイン関数でその結果を表示

--- hello.hs
+++ hello.hs
@@ -1,4 +1,8 @@
-x = 5
-x = 10
+safeDivide :: Double -> Double -> Maybe Double
+safeDivide _ 0 = Nothing -- 0 で割る場合は結果を Nothing にする
+safeDivide x y = Just (x / y) -- 正常は割り算の結果を Just で包む
+main :: IO ()
+main = do
+    print (safeDivide 10 2) -- Just 5.0
+    print (safeDivide 10 0) -- Nothing
 
-y = x + 5
