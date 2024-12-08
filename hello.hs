73c04a836af014509120acd0a9a24dc30eee0005
TUCCHI1 <tutikikin1@gmail.com>
2024-12-08 18:08:28 +0900

    hello.hsからタプル関連のコードを削除し、xとyの変数を追加

--- hello.hs
+++ hello.hs
@@ -1,18 +1,4 @@
-myTuple :: (Int, String, Bool)
-myTuple = (42, "Haskell", True)
+x = 5
+x = 10
 
-firstElement = fst (42, "Haskell")
-secondElement = snd (42, "Haskell")
-
-main :: IO ()
-main = do
-    -- リストの各要素を２倍にする
-    let list = [1, 2, 3, 4, 5]
-    let doubled = map (*2) list
-    print doubled
-    -- リストの偶数要素のみを取り出す
-    let evens = filter even list
-    print evens
-    -- リストの合計を計算する
-    let sumList = sum list
-    print sumList
+y = x + 5
