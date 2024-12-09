b9612dec84c7fa6876318ab583baef92b1420f18
TUCCHI1 <tutikikin1@gmail.com>
2024-12-09 12:18:24 +0900

    hello.hsから安全な割り算関数を削除し、ユーザーから名前を取得して挨拶する機能を追加

--- hello.hs
+++ hello.hs
@@ -1,12 +1,5 @@
-safeDiv :: Double -> Double -> Maybe Double
-safeDiv _ 0 = Nothing -- 0 である場合は Nothing を返す
-safeDiv x y = Just (x / y)
-
 main :: IO ()
 main = do
-    let a = 10
-    let b = 0
-    let result = safeDiv a b
-    case result of
-        Nothing -> putStrLn "Error: Division by zero"
-        Just value -> putStrLn $ "Result: " ++ show value
+    putStrLn "Enter your name."
+    name <- getLine
+    putStrLn ("Hello, " ++ name ++ "!")
