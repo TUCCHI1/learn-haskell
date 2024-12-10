1ffb6bd72f775b17cc850bbca1fd4c431f6458d0
TUCCHI1 <tutikikin1@gmail.com>
2024-12-10 23:24:23 +0900

    hello.hsのメイン関数を修正し、1から10までの数値のリストを生成し、偶数を抽出して表示する機能を追加

--- hello.hs
+++ hello.hs
@@ -1,11 +1,10 @@
-data Color = Red | Green | Blue deriving Show
-
-describeColor Red = "The color is Red!"
-describeColor Green = "The color is Green!"
-describeColor Blue = "The color is Blue!"
-
-main :: IO ()
+-- 1から10までの数値のリストを生成
+numbers = [1..10]
+-- 1から10までの数値を２倍にしたリストを生成
+doubledNumbers = [x * 2 | x <- numbers]
+-- 偶数のみを抽出したリストを生成
+evenNumbers = [x | x <- numbers, x `mod` 2 == 0]
+-- 結果を出力
 main = do
-    putStrLn $ describeColor Red
-    putStrLn $ describeColor Green
-    putStrLn $ describeColor Blue
+    print doubledNumbers
+    print evenNumbers
