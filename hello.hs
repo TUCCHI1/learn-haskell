4fa3c6a831016c2a94d6b29ce5fe76b5996ad15e
TUCCHI1 <tutikikin1@gmail.com>
2024-12-10 23:27:56 +0900

    hello.hsのメイン関数を修正し、偶数の抽出方法を改善

--- hello.hs
+++ hello.hs
@@ -3,7 +3,7 @@ numbers = [1..10]
 -- 1から10までの数値を２倍にしたリストを生成
 doubledNumbers = [x * 2 | x <- numbers]
 -- 偶数のみを抽出したリストを生成
-evenNumbers = [x | x <- numbers, x `mod` 2 == 0]
+evenNumbers = [x | x <- numbers, even x]
 -- 結果を出力
 main = do
     print doubledNumbers
