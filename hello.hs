32b656788b06c53b863e61b9f7271046c9170bbc
TUCCHI1 <tutikikin1@gmail.com>
2024-12-11 14:33:35 +0900

    hello.hsのメイン関数を修正し、1から10までの数値を2倍にしたリストを生成する機能を追加

--- hello.hs
+++ hello.hs
@@ -1,10 +1,9 @@
--- 1から10までの数値のリストを生成
 numbers = [1..10]
--- 1から10までの数値を２倍にしたリストを生成
+
 doubledNumbers = [x * 2 | x <- numbers]
--- 偶数のみを抽出したリストを生成
+
 evenNumbers = [x | x <- numbers, even x]
--- 結果を出力
+
 main = do
     print doubledNumbers
     print evenNumbers
