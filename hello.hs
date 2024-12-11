24a3dfb5e36a902bc325bfc611b2af91d7d77d56
TUCCHI1 <tutikikin1@gmail.com>
2024-12-11 14:46:26 +0900

    hello.hsにPersonデータ型を追加し、Eqインスタンスを実装して等価性を比較できるようにした

--- hello.hs
+++ hello.hs
@@ -1,9 +1,11 @@
-numbers = [1..10]
+data Person = Person String Int deriving Show
 
-doubledNumbers = [x * 2 | x <- numbers]
-
-evenNumbers = [x | x <- numbers, even x]
+instance Eq Person where
+    (Person name1 age1) == (Person name2 age2) = (name1 == name2) && (age1 == age2)
 
 main = do
-    print doubledNumbers
-    print evenNumbers
+    let person1 = Person "Alice" 30
+    let person2 = Person "Alice" 30
+    let person3 = Person "Bob" 25
+    print (person1 == person2)
+    print (person1 == person3)
