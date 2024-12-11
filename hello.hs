9a84eb5238c13eb245285558c27315b834acf54e
TUCCHI1 <tutikikin1@gmail.com>
2024-12-11 15:58:12 +0900

    Personデータ型のShowインスタンスを修正し、型クラスの継承を明示化

--- hello.hs
+++ hello.hs
@@ -1,4 +1,4 @@
-data Person = Person String Int deriving Show
+data Person = Person String Int deriving (Show)
 
 instance Eq Person where
     (Person name1 age1) == (Person name2 age2) = (name1 == name2) && (age1 == age2)
@@ -8,4 +8,5 @@ main = do
     let person2 = Person "Alice" 30
     let person3 = Person "Bob" 25
     print (person1 == person2)
-    print (person1 == person3)
+    print (person1 == person3) 
+
